76a3ae744aece1433b98cece909a92e909992b2d
AvitusI <avitussweetbert213@gmail.com>
2025-05-09 01:13:57 +0300

    fire

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-"use client"                   
+"use client"
 
+import { Suspense } from "react"
 import { useSearchParams } from "next/navigation"
 
 export default function ErrorPage() {
@@ -15,22 +16,24 @@ export default function ErrorPage() {
     const type = searchParams.get("type")
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            {type === "register_mail_exists" && (
-                <strong>
-                    This user already exists. Please try logging in instead. 
-                </strong>
-            )}
-            {type === "register_unknown" && (
-                <strong>
-                    An unknown error occurred while registering. Please try again later.
-                </strong>
-            )}
-            {type === "login-failed" && (
-                <strong>
-                    Aunthentication failed. Please try again later.
-                </strong>
-            )}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex flex-col items-center justify-center h-screen">
+                {type === "register_mail_exists" && (
+                    <strong>
+                        This user already exists. Please try logging in instead.
+                    </strong>
+                )}
+                {type === "register_unknown" && (
+                    <strong>
+                        An unknown error occurred while registering. Please try again later.
+                    </strong>
+                )}
+                {type === "login-failed" && (
+                    <strong>
+                        Aunthentication failed. Please try again later.
+                    </strong>
+                )}
+            </div>
+        </Suspense>
     )
 }
