ecf717493e96d03a95df502264a0a151892cd212
AvitusI <avitussweetbert213@gmail.com>
2025-05-09 00:53:23 +0300

    again

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,9 +1,8 @@
 import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient";
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: NextApiRequest) {
-  const supabase = getSupabaseCookiesUtilClient();
+export async function GET(request: NextRequest) {
+  const supabase = await getSupabaseCookiesUtilClient();
   await supabase.auth.signOut();
   return NextResponse.redirect(new URL("/", request.url));
 }
